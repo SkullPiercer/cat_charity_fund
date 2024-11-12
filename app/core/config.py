3e8b2de881679b693bf1abbafa35be1ed7aaba9c
Alexander <akatsuki_sanya@vk.com>
2024-11-12 13:48:43 +0500

    Initial commit. Skelet of project is Done

---
+++ app/core/config.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from pydantic import BaseSettings, EmailStr
+
+
+class Settings(BaseSettings):
+    app_title: str = 'Проект ЯП'
+    app_description: str = 'Описание'
+    database_url: str
+    secret: str = 'SECRET'
+    first_superuser_email: Optional[EmailStr] = None
+    first_superuser_password: Optional[str] = None
+
+    class Config:
+        env_file = '.env'
+
+
+settings = Settings()
