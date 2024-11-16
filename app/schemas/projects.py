ef87c85b6ce22cd40adcca6d9f180a5f9c0d800b
Alexander <akatsuki_sanya@vk.com>
2024-11-16 20:09:00 +0500

    Made name and description validation

--- app/schemas/projects.py
+++ app/schemas/projects.py
@@ -12,13 +12,25 @@ class CharityProjectUpdate(BaseModel):
 
 
 class CharityProjectCreate(BaseModel):
-    name: str
-    description: str
+    name: str = Field(min_length=1, max_length=100)
+    description: str = Field(min_length=1)
     full_amount: int = Field(..., gt=0)
     invested_amount: int = 0
     fully_invested: bool = False
     create_date: datetime = datetime.now()
     close_date: datetime = None
+
+    @validator('name')
+    def name_cannot_be_null(cls, value):
+        if value is None:
+            raise ValueError('Имя проекта не может быть пустым!')
+        return value
+
+    @validator('description')
+    def desc_cannot_be_null(cls, value):
+        if value is None:
+            raise ValueError('Описание проекта не может быть пустым!')
+        return value
 
 class CharityProjectDB(CharityProjectCreate):
     id: int
