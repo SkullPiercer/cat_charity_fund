77b5ae99a83487f18ce677565438e50b438caaea
Alexander <akatsuki_sanya@vk.com>
2024-11-17 21:08:32 +0500

    Remade all donations get endpoint

--- app/api/endpoints/donations.py
+++ app/api/endpoints/donations.py
@@ -4,9 +4,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import User
 from app.core.db import get_async_session
-from app.schemas.donation import DonationDB, DonationCreate, DonationUserSchema, DonationAdminSchema
+from app.schemas.donation import DonationDB, DonationCreate, DonationUserSchema
 from app.crud.donation import donation_crud
-from app.core.user import current_user
+from app.core.user import current_user, current_superuser
 from app.core.utils import invest_funds
 
 router = APIRouter()
@@ -46,17 +46,11 @@ async def get_all_donation(
 @router.get(
     '/',
     response_model=list[DonationDB],
-    response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)]
 )
 async def get_all_donation(
     session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_user)
 ):
-    if user.is_superuser:
-        all_projects = await donation_crud.get_multi(session)
-        return all_projects
-    all_projects = await donation_crud.get_by_user(
-        session=session, user=user
-    )
+    all_projects = await donation_crud.get_multi(session)
     return all_projects
 
--- app/schemas/donation.py
+++ app/schemas/donation.py
@@ -10,13 +10,13 @@ class DonationCreate(BaseModel):
     comment: Optional[str] = None
     full_amount: int = Field(..., gt=0)
     invested_amount: int = 0
-    fully_invested: bool = False
+    fully_invested: Optional[bool] = False
     create_date: datetime = Field(default_factory=datetime.now)
     close_date: datetime = None
 
 class DonationDB(DonationCreate):
     id: int
-
+    user_id: Optional[int]
     class Config:
         orm_mode = True
 
@@ -29,9 +29,3 @@ class DonationUserSchema(BaseModel):
     class Config:
         orm_mode = True
 
-
-class DonationAdminSchema(DonationUserSchema):
-    user_id: int
-    invested_amount: int
-    fully_invested: bool
-    close_date: Optional[datetime]
