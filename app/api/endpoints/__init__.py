3311e7e75e6df59caa9d81adb964deeaa376da45
Alexander <akatsuki_sanya@vk.com>
2024-11-29 21:57:20 +0500

    Lets try with all

--- app/api/endpoints/__init__.py
+++ app/api/endpoints/__init__.py
@@ -1,3 +1,6 @@
 from .user import router as user_router
 from .projects import router as projects_router
 from .donations import router as donations_router
+
+
+__all__ = ["user_router", "projects_router", "donations_router"]
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,6 @@
 from .user import User
 from .charity_project import CharityProject
 from .donation import Donation
+
+
+__all__ = ["User", "CharityProject", "Donation"]
